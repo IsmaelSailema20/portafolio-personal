f0b3654356fa7049a48aac725e851e2cf9877705
IsmaelSailema20 <ismaelsailema110@gmail.com>
2024-11-15 20:16:59 -0500

    Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://IsmaelSailema20.github.io',
+  base: 'portafolio-personal',
 });
